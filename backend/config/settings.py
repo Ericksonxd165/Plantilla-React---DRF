95bd4b9ac7d4e2dcf39d438c0aea158d7bbab7c4
Erick Pereira <ericksonxd_164@hotmail.com>
2025-07-15 09:48:28 -0400

    arreglo sin docker

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -98,13 +98,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        # ...
+    
+    
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
